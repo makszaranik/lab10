e1c4276e636dd58fe1a830f452ca8c2b3511cf5c
makszaranik <52628168+makszaranik@users.noreply.github.com>
2024-05-09 12:11:54 +0300

    remove set form Main in 8 variant

--- src/lab10/lab8Variant/Main.java
+++ src/lab10/lab8Variant/Main.java
@@ -14,7 +14,6 @@ public class Main {
     Car car1 = new Car(350, "Lamborghini", "red");
     Car car2 = new Car(250, "Ferrari", "blue");
     Car[] carArray = {car1, car2};
-
     Iterable<Car> cars = Arrays.asList(carArray);
     cars.forEach(System.out::println);
 
@@ -22,24 +21,15 @@ public class Main {
     Ship ship1 = new Ship(200, 3000);
     Ship ship2 = new Ship(150, 2000);
     Ship[] shipArray = {ship1, ship2};
-
     Iterable<Ship> ships = Arrays.asList(shipArray);
     ships.forEach(System.out::println);
+
 
     Document document1 = new TextDocument("document1");
     Document document2 = new TextDocument("document2 is larger than document1");
     Document[] documentArray = {document1, document2};
-
     Iterable<Document> documents = Arrays.asList(documentArray);
     documents.forEach(System.out::println);
 
-    TreeSet<TextDocument> textDocumentSet = new TreeSet<>(Comparator.nullsLast(Comparator.comparing(TextDocument::getAmountSymbols)));
-    textDocumentSet.add(new TextDocument("Hello"));
-    textDocumentSet.add(new TextDocument("Welcome to the Java world"));
-    textDocumentSet.add(new TextDocument("Short"));
-
-    Iterable<TextDocument> textDocuments = textDocumentSet;
-    System.out.println("TextDocuments:");
-    textDocuments.forEach(System.out::println);
   }
 }
